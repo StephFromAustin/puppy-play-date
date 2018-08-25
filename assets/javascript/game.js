091226a7b282124337b6dcb43496e03dc72267ab
KSG Sounds Macbook Pro <ksgsound@ksgs-mbp.attlocal.net>
2018-08-24 23:24:49 -0500

    html random number fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ $( document ).ready(function(){
   // RANDOM NUMBER GENERATOR 
   let Random=Math.floor(Math.random()*100+10)
   
-        // DISPLAY THE RANDOM NUMBER ((** WRITE HTML? FIND FIX))
+        // DISPLAY THE RANDOM NUMBER ((** WRITE HTML? FIND FIX!! TEXT?))
   $('.numberToMatch').text(Random);
   
   
